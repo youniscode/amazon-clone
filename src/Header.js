fafd614a16eb7cdd6ec5f0c7cb840c483c8eb3c7
Younis <youniscode@gmail.com>
2021-11-16 16:30:33 +0100

    createdHeaderSearchbarBasketLogo

---
+++ src/Header.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+
+
+function Header() {
+  return (
+    <div className="header">
+      {/*AMAZON LOGO*/}
+      <img className="header__logo" src="https://www.sahirkarani.com/images/amazon.png" alt="logo_amazon" />
+
+      {/*SEARCH BAR*/}
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__serachIcon" />
+      </div>
+
+      <div className="header__nav">
+
+        <div className="header__option">
+          <span className="header__optionLineOn">Hello Guest</span>
+          <span className="header__optionLineTw"> Sign In</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOn">Returns</span>
+          <span className="header__optionLineTw">& Orders</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOn">Your</span>
+          <span className="header__optionLineTw">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__optionCount">0</span>
+        </div>
+
+
+
+      </div>
+    </div>
+
+  )
+}
+
+export default Header
