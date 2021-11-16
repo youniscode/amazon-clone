7cae870ca1cf15ef5bf89545d661db662a37ada4
Younis <youniscode@gmail.com>
2021-11-16 16:27:46 +0100

    createdHeaderWithLogoSearchbarBasket

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD7_N-oEug9IGm2GLH0s6zNfsusGqVXFh4",
+  authDomain: "amawon-clone.firebaseapp.com",
+  projectId: "amawon-clone",
+  storageBucket: "amawon-clone.appspot.com",
+  messagingSenderId: "595313124805",
+  appId: "1:595313124805:web:6364e4b82336247b5c97ac",
+  measurementId: "G-RRWEPF55XN"
+};
